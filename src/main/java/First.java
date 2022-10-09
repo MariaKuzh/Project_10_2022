712471b1b9b8cdb0b8858b56ec7cf436f5bb1ad6
Maria Kuzhakova <mykuzhakova@gmail.com>
2022-10-08 23:47:55 -0400

    Initial commit

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,5 +1,9 @@
 public class First {
     public static void main(String[] args) {
         System.out.println("Hi");
+<<<<<<< HEAD
+=======
+        System.out.println("Hello world");
+>>>>>>> 357938b (Initial commit)
     }
 }
